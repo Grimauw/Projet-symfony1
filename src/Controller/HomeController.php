f88ad3c2f7b9c5c2cefaa9feefac42115c329c82
Afpa <Afpa@POR7302503.exchange.ad.afpanet>
2022-05-04 10:12:39 +0200

    DI + Factorisation

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    private $repoArticle;
+
+    public function __construct(ArticleRepository $repoArticle)
+    {
+        $this->repoArticle = $repoArticle;
+    }
+/**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        $articles = $this->repoArticle->findAll();
+        return $this->render('home/index.html.twig', [
+            "articles" => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    public function show(Article $article): Response
+    {
+        if (!$article) {
+            $this->redirectToRoute("home");
+        }
+        return $this->render(' home/show.html.twig', [
+            "article" => $article
+        ]);
+    }
+}
